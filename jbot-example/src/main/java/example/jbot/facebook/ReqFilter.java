4df4462a244837515a1fb1754c047491cdc4fd9f
qingzhou.zheng <qingzhou.zheng@ck-telecom.com>
2019-02-28 18:09:43 +0800

    [Feature]...

---
+++ jbot-example/src/main/java/example/jbot/facebook/ReqFilter.java
@@ -0,0 +1,30 @@
+package example.jbot.facebook;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ReqFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods",
+                "POST, GET, OPTIONS");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+}
